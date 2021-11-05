014096709160947a85002f6b599af3f8df20f619
pqnogeek <francoeu.me@gmail.com>
2021-11-05 19:09:58 -0300

    desafio concluído

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -10,6 +10,15 @@ class ListAllUsersUseCase {
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (typeof user !== "undefined") {
+      if (!user.admin) {
+        throw new Error("No permission");
+      }
+      return this.usersRepository.list();
+    }
+    throw new Error("User not found!");
   }
 }
 
